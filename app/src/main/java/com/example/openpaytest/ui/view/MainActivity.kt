d979fe460c777531ecd4281dd0adebb995527b7b
smmstori <saul.mendez@storicard.com>
2023-11-13 07:06:45 -0600

    BottomNavigationView added

--- app/src/main/java/com/example/openpaytest/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/openpaytest/ui/view/MainActivity.kt
@@ -2,22 +2,41 @@ package com.example.openpaytest.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
-import com.example.openpaytest.R.layout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.openpaytest.R
 import com.example.openpaytest.databinding.ActivityMainBinding
-import com.example.openpaytest.ui.view.profile.ProfileViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val profileViewModel by viewModels<ProfileViewModel>()
-
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.navView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_profile -> {
+                    navController.navigate(R.id.nav_profile)
+                    true
+                }
+                R.id.nav_movies -> {
+                    navController.navigate(R.id.nav_movies)
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.navView.selectedItemId = R.id.nav_profile
+
     }
+
 }
